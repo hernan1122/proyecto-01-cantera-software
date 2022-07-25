834858f4a81935bdf1cf6ce471e07ee423e89235
Axel Torrico <torricohernan2@gmail.com>
2022-07-25 14:50:46 -0500

    mi primer commit

---
+++ algoritmos.js
@@ -0,0 +1,65 @@
+let a = 5
+let b = 7
+
+function suma(a,b){
+    return (a + b);
+}
+
+
+var miNombre = "Axel ";
+var miApellido = "Torrico";
+
+function nombre(){
+return (miNombre + miApellido);
+}
+
+
+var edad = 18
+
+if(edad === 18) {
+    console.log("Puedes viajar");
+} else {
+    console.log("Aun no puedes viajar");
+}
+
+
+var numero = 5;
+
+switch (numero) {
+    case 5:
+        console.log("Soy 5");
+        break;
+    case 50:
+        console.log("Soy 50");
+        break;
+    case 500:
+        console.log("Soy 500");
+        break;
+    default:
+        console.log("No soy nada");
+}
+
+
+let n1 = 5;
+let n2 = 4;
+
+const sumar = n1 + n2;
+
+if (resultado > 9) {
+    return console.log ("el valor es mayor que 9")
+} else {
+    return console.log ("el valor es menor que 9")
+}
+
+function resultado (){
+    let n1 = 5;
+    let n2 = 4;
+    
+    const suma = n1 + n2;
+    
+    if (resultado > 9) {
+        return console.log ("el valor es mayor que 9")
+    } else {
+        return console.log ("el valor es menor que 9")
+    }   
+}
